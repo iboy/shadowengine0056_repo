b443706fb365348fbd435cdd37ba176ac7e281cf
iboy <ian.grant@mac.com>
2017-05-09 22:21:06 +0100

    First Commit of duplicate of shadowengine_0055_repo. Updating to Unity 5.6

---
+++ Assets/Scripts/ScaleMe2D.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using HedgehogTeam.EasyTouch;
+
+public class ScaleMe2D : MonoBehaviour {
+
+	public GameObject target;
+	public void ScaleWithDelta(Gesture gesture) {
+
+		if (gesture.pickedObject) {
+		
+			if (gesture.pickedObject.name == "Controller 1" ) { // select intended target - requires naming consistency
+				
+				target.transform.localScale += new Vector3( gesture.deltaPinch* .4f * Time.deltaTime,  gesture.deltaPinch* .4f * Time.deltaTime,  0);
+				//target.transform.localScale += new Vector3( gesture.deltaPinch* .4f * Time.deltaTime,  gesture.deltaPinch* .4f * Time.deltaTime,  1);
+				//Debug.Log("You're scaling Controller 1");
+
+			} else {
+				
+				// pinching in space
+				//Debug.Log("Picked Object:"+gesture.pickedObject.name+" on layer "+gesture.pickedObject.layer);
+		
+			}
+
+		}
+	}
+
+}
