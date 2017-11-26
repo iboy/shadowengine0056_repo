8f1d4a49bfd82062a89769519cf4089509729fca
iboy <ian.grant@mac.com>
2017-11-26 22:11:13 +0000

    tweaks

--- Assets/Scripts/ScaleMe2D.cs
+++ Assets/Scripts/ScaleMe2D.cs
@@ -5,14 +5,15 @@ using HedgehogTeam.EasyTouch;
 public class ScaleMe2D : MonoBehaviour {
 
 	public GameObject target;
+    public float scaleMultiplier = 0.4f;
 	public void ScaleWithDelta(Gesture gesture) {
 
 		if (gesture.pickedObject) {
 		
 			if (gesture.pickedObject.name == "Controller 1" ) { // select intended target - requires naming consistency
 				
-				target.transform.localScale += new Vector3( gesture.deltaPinch* .4f * Time.deltaTime,  gesture.deltaPinch* .4f * Time.deltaTime,  0);
-				//target.transform.localScale += new Vector3( gesture.deltaPinch* .4f * Time.deltaTime,  gesture.deltaPinch* .4f * Time.deltaTime,  1);
+				target.transform.localScale += new Vector3( gesture.deltaPinch* scaleMultiplier * Time.deltaTime,  gesture.deltaPinch* scaleMultiplier * Time.deltaTime,  0);
+				//target.transform.localScale += new Vector3( gesture.deltaPinch* scaleMultiplier * Time.deltaTime,  gesture.deltaPinch* scaleMultiplier * Time.deltaTime,  1);
 				//Debug.Log("You're scaling Controller 1");
 
 			} else {
